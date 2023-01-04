b6331bca842f9e723f63bd7a12de2bd5e730e93d
zygisG13 <zygimantas.guobuzas@gmail.com>
2023-01-04 16:11:48 +0200

    Display number of more signs

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,33 @@
 function display(number) {
-  const show = document.querySelector(".display").textContent = number;
-  
+  if (number[11] !== undefined) {
+    document.querySelector(".display").textContent = "NaN";
+    number = [];
+  } else {
+    document.querySelector(".display").textContent = number.join("");
+  }
 }
 
+let number1 = [];
+let number2 = [];
+let index = 0;
 
-
-const numbers = document.querySelectorAll(".item.numbers");
-
-numbers.forEach((number) => {
+const signs = document.querySelectorAll(".item");
+signs.forEach((sign) => {
   // and for each one we add a 'click' listener
-  number.addEventListener("click", () => {
-    display(number.textContent);
+  sign.addEventListener("click", () => {
+    if (sign.className !== "item signs" || number1 === []) {
+      console.log(sign.className);
+      number1[index] = sign.textContent;
+      display(number1);
+      index++;
+    } else {
+    }
   });
 });
 
-const signs = document.querySelectorAll(".item.signs");
-
-signs.forEach((sign) => {
+/*signs.forEach((sign) => {
   // and for each one we add a 'click' listener
   sign.addEventListener("click", () => {
     console.log(sign.textContent);
   });
-});
+});*/
