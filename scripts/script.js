3947420cf71c59cd1ef208ebb7e4b6d614e330d2
zygisG13 <zygimantas.guobuzas@gmail.com>
2023-01-05 10:47:27 +0200

    Script fills number1 sing and number1, also displays numbers.

--- scripts/script.js
+++ scripts/script.js
@@ -9,25 +9,36 @@ function display(number) {
 
 let number1 = [];
 let number2 = [];
-let index = 0;
+let index1 = 0;
+let index2 = 0;
+let sign = "";
 
-const signs = document.querySelectorAll(".item");
-signs.forEach((sign) => {
+const symbols = document.querySelectorAll(".item");
+symbols.forEach((symbol) => {
   // and for each one we add a 'click' listener
-  sign.addEventListener("click", () => {
-    if (sign.className !== "item signs" || number1 === []) {
-      console.log(sign.className);
-      number1[index] = sign.textContent;
+  symbol.addEventListener("click", () => {
+    //checking if pushed is number, and sign should be empty. Fill number1 array
+    console.log(number1);
+    if (symbol.className !== "item signs" && sign === "") {
+      console.log(symbol.className);
+      number1[index1] = symbol.textContent;
       display(number1);
-      index++;
-    } else {
+      index1++;
+    }
+    //checking if pushed symbol is not number and number1 array is full. We expecting sign
+    if (symbol.className !== "item numbers" && number1.length !== 0) {
+      console.log(symbol.className);
+      sign = symbol.textContent;
+      console.log(sign);
+    }
+    //checking if pushed is number, and sign should be not empty, number1 should have entries. Fill number2 array
+    if (symbol.className !== "item signs" && sign !== "" && number1.length !== 0) {
+      console.log(symbol.className);
+      number2[index2] = symbol.textContent;
+      display(number2);
+      index2++;
     }
   });
 });
 
-/*signs.forEach((sign) => {
-  // and for each one we add a 'click' listener
-  sign.addEventListener("click", () => {
-    console.log(sign.textContent);
-  });
-});*/
+
