c23a7849003e2965ef7a2bbcf6b030f7eeebba4b
zygisG13 <zygimantas.guobuzas@gmail.com>
2023-01-05 12:56:18 +0200

    change '=' class, sum functionality works

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,40 @@
 function display(number) {
+  //This function shows entries in web page display "<div>"
   if (number[11] !== undefined) {
     document.querySelector(".display").textContent = "NaN";
     number = [];
   } else {
     document.querySelector(".display").textContent = number.join("");
   }
+}
+
+function operate(firstNum, operator, secondNum) {
+  //Decides which math action should be taken and return result
+  if (operator === "+") {
+    number1 = Array.from(String(add(firstNum, secondNum)));
+    display(number1);
+    return number1, (number2 = []);
+  }
+  if (operator === "-") {
+    display(subtract(firstNum, secondNum));
+    return subtract(firstNum, secondNum);
+  }
+  if (operator === "x") {
+    display(multiple(firstNum, secondNum));
+    return multiple(firstNum, secondNum);
+  }
+  if (operator === "x") {
+    display(multiple(firstNum, secondNum));
+    return multiple(firstNum, secondNum);
+  }
+  if (operator === "/") {
+    display(divide(firstNum, secondNum));
+    return divide(firstNum, secondNum);
+  }
+}
+
+function add(firstNum, secondNum) {
+  return firstNum + secondNum;
 }
 
 let number1 = [];
@@ -18,27 +48,32 @@ symbols.forEach((symbol) => {
   // and for each one we add a 'click' listener
   symbol.addEventListener("click", () => {
     //checking if pushed is number, and sign should be empty. Fill number1 array
-    console.log(number1);
     if (symbol.className !== "item signs" && sign === "") {
-      console.log(symbol.className);
       number1[index1] = symbol.textContent;
       display(number1);
       index1++;
     }
     //checking if pushed symbol is not number and number1 array is full. We expecting sign
     if (symbol.className !== "item numbers" && number1.length !== 0) {
-      console.log(symbol.className);
       sign = symbol.textContent;
-      console.log(sign);
     }
     //checking if pushed is number, and sign should be not empty, number1 should have entries. Fill number2 array
     if (symbol.className !== "item signs" && sign !== "" && number1.length !== 0) {
-      console.log(symbol.className);
       number2[index2] = symbol.textContent;
       display(number2);
       index2++;
     }
+    //checking if pushed is number, and sign should be not empty, number1 should have entries. Fill number2 array
+    if (symbol.className !== "item numbers" && number1.length !== 0 && number2.length !== 0) {
+      let int1 = parseFloat(number1.join(""));
+      let int2 = parseFloat(number2.join(""));
+      operate(int1, sign, int2);
+    }
+    //checking if '=' is pushed, and sign should be not empty, number1 should have entries. Fill number2 array
+    if (symbol.className === "item equal" && number1.length !== 0 && number2.length !== 0) {
+      let int1 = parseFloat(number1.join(""));
+      let int2 = parseFloat(number2.join(""));
+      operate(int1, sign, int2);
+    }
   });
 });
-
-
